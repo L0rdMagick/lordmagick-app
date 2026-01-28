4d55eb5be854c12244c8e9c9634bc56169b76708
Daniel Guerrero <massivebrains@gmail.com>
2026-01-28 12:00:52 -0600

    digital servitor

--- src/app/components/MusicPlayer.tsx
+++ src/app/components/MusicPlayer.tsx
@@ -175,6 +175,7 @@ const MusicPlayer = () => {
                 {isVisible && (
                     !isExpanded ? (
                         <motion.button
+                            key="minimized"
                             initial={{ scale: 0, opacity: 0 }}
                             animate={{ scale: 1, opacity: 1 }}
                             exit={{ scale: 0, opacity: 0 }}
@@ -187,6 +188,7 @@ const MusicPlayer = () => {
                         </motion.button>
                     ) : (
                         <motion.div
+                            key="expanded"
                             initial={{ opacity: 0, y: 20, scale: 0.9 }}
                             animate={{ opacity: 1, y: 0, scale: 1 }}
                             exit={{ opacity: 0, y: 20, scale: 0.9 }}
