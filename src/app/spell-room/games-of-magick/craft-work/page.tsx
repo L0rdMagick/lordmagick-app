8d7107ebba10019e8f2eea525f718f9ce3ddddd5
Daniel Guerrero <massivebrains@gmail.com>
2026-02-17 09:43:38 -0600

    craft work

--- src/app/spell-room/games-of-magick/craft-work/page.tsx
+++ src/app/spell-room/games-of-magick/craft-work/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function CraftWorkPage() {
     return (
-        <div className="w-full h-full flex items-center justify-center bg-black overflow-hidden relative">
+        <div className="w-full h-[calc(100vh-80px)] md:h-[calc(100vh-100px)] flex items-center justify-center bg-black overflow-hidden relative">
             <iframe 
                 src="/games/craft-work.html" 
                 className="w-full h-full border-none"
