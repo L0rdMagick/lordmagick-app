2f61d8a187a996338ef3327a54bcca76ba3d3770
Daniel Guerrero <massivebrains@gmail.com>
2026-02-24 04:23:47 -0600

    Patch Map editor canvas trailing bug rendering looping. Add encodeURI to Node loader script correctly wrapping spaces inside image paths. Wrap drawImage in protective try/catch logic to ensure oCtx.restore() operates sequentially and correctly protects global renderer loop matrix offsets.

---
+++ generate_assets.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+const getFiles = (dir) => {
+    let results = [];
+    const list = fs.readdirSync(dir);
+    list.forEach(file => {
+        file = dir + '/' + file;
+        const stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) {
+            results = results.concat(getFiles(file));
+        } else if (path.extname(file) === '.png') {
+            results.push(file);
+        }
+    });
+    return results;
+};
+
+const assets = getFiles('./public/images/craft-work');
+let spritesData = [];
+
+assets.forEach(p => {
+    const relPath = encodeURI(p.replace('./public/', '../../'));
+    const name = path.basename(p, '.png');
+    for(let y=0; y<4; y++) {
+        for(let x=0; x<4; x++) {
+            spritesData.push({ 
+                id: name + '_' + x + '_' + y, 
+                path: relPath, 
+                sx: x*512, 
+                sy: y*512, 
+                sw: 512, 
+                sh: 512 
+            });
+        }
+    }
+});
+
+const baseStr = 'const AVAILABLE_ASSETS = ' + JSON.stringify(spritesData) + ';\nif(typeof module !== "undefined") try { module.exports = { AVAILABLE_ASSETS }; } catch(e) {}';
+fs.writeFileSync('./public/games/map-editor/assetList.js', baseStr);
+console.log('Wrote ' + spritesData.length + ' sprites');
